78bf017873c4ed1aaf3a7a85584bf679e3af9ac1
harsh2000yadav <harsh2000yadav@gmail.com>
2020-12-21 11:39:33 +0530

    ProjectSummary

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Name</span>
+                <p>Posted By Harsh Yadav</p>
+                <p className="grey-text">20th December 2020</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary
