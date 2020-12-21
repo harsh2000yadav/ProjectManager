75ee724f2acb201c4879bee7505d0834696300fb
harsh2000yadav <harsh2000yadav@gmail.com>
2020-12-21 11:59:06 +0530

    ProjectDetails

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProjectDetails() {
+    return (
+        <div className="container section project-details">
+            <div className="card">
+                <div className="card-content">
+                    <span className="card-title">Project Title</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem tempore neque consequatur amet obcaecati distinctio voluptatem aliquid modi cum eos tenetur vero aspernatur, nesciunt hic at omnis dolores et commodi!</p>
+                </div>
+                <div className="card-action grey lighten-4 black-text">
+                    <div>Posted by Harsh Yadav</div>
+                    <div>21st December 2020</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = () => {
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">Project Name</span>
                 <p>Posted By Harsh Yadav</p>
-                <p className="grey-text">20th December 2020</p>
+                <p className="grey-text">21st December 2020</p>
             </div>
         </div>
     )
