cb1df731d676b6d818c95302c8a574e1638816ba
harsh2000yadav <harsh2000yadav@gmail.com>
2020-12-24 21:11:02 +0530

    Firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,6 @@
 export const createProject = (project) => {
     
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         //async calls to database and pausing dispatch for the moment 
         dispatch({
             type: 'ADD_PROJECT',
