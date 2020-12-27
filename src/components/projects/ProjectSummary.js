159be484e3e15e5ab89d11a1b1ddeca1ac930f09
harsh2000yadav <harsh2000yadav@gmail.com>
2020-12-27 11:50:26 +0530

    Auth

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -14,18 +14,3 @@ const ProjectSummary = ({project}) => {
 
 export default ProjectSummary
 
-import React from 'react'
-
-const ProjectSummary = ({project}) => {
-    return (
-        <div className="card project-summary radius-3">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">{project.title}</span>
-                <p>Posted By {project.authorFirstName} {project.authorLastName}</p>
-                <p className="grey-text">21st December 2020</p>
-            </div>
-        </div>
-    )
-}
-
-export default ProjectSummary
