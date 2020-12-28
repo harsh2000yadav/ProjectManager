e866bc608fb74d699b5f00cd500b7732c4515ccb
harsh2000yadav <harsh2000yadav@gmail.com>
2020-12-28 21:28:34 +0530

    Auth Actions

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -16,3 +16,22 @@ const signIn = (credentials) =>{
         })
     }
 } 
+
+const signIn = (credentials) =>{
+    return (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+        
+
+        firebase.auth().signInWithEmailAndPassword(
+            credentials.email, credentials.password
+        ).then(() =>{
+            dispatch(
+                {type: 'LOGIN_SUCCESS'}
+            )
+        }).catch(() =>{
+            dispatch({
+                type: 'LOGIN_FAIL'
+            })
+        })
+    }
+} 
