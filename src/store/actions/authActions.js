81954482b7d081489903d614a4edf78c5c5d0601
harsh2000yadav <harsh2000yadav@gmail.com>
2020-12-28 21:34:34 +0530

    Project List

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -17,21 +17,3 @@ const signIn = (credentials) =>{
     }
 } 
 
-const signIn = (credentials) =>{
-    return (dispatch, getState, {getFirebase}) => {
-        const firebase = getFirebase();
-        
-
-        firebase.auth().signInWithEmailAndPassword(
-            credentials.email, credentials.password
-        ).then(() =>{
-            dispatch(
-                {type: 'LOGIN_SUCCESS'}
-            )
-        }).catch(() =>{
-            dispatch({
-                type: 'LOGIN_FAIL'
-            })
-        })
-    }
-} 
