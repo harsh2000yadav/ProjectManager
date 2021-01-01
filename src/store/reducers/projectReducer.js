a9bcf1aca276dae6bf19d918455201ddd1a8cd73
Harsh <32234204+harsh2000yadav@users.noreply.github.com>
2021-01-01 21:33:13 +0530

    update projectReducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -24,3 +24,30 @@ const projectReducer = (state = initState, action) =>{
 }
 
 export default projectReducer
+
+const initState = {
+    projects : [
+        {id: '1', title: 'Test Project 1', desc: 'Description of test project 1'},
+        {id: '2', title: 'Test Project 2', desc: 'Description of test project 2'},
+        {id: '3', title: 'Test Project 3', desc: 'Description of test project 3'}
+
+    ]
+
+}
+
+const projectReducer = (state = initState, action) =>{
+    switch (action.type) {
+        case 'ADD_PROJECT': 
+            console.log('New project created', action.project)   
+            return state;        
+        
+        case 'ADD_PROJECT_ERROR':
+            console.log('Error while creating project', action.err)
+            return state;
+    
+        default:
+            return state;
+    }
+}
+
+export default projectReducer
