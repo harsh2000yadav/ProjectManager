c938dcf1aaac1fe3c48ea53ae1df1afab43d3a34
Harsh <32234204+harsh2000yadav@users.noreply.github.com>
2021-01-03 21:45:39 +0530

    Update SignedInLinks.js

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -13,17 +13,3 @@ const SignedInLinks = ()=>{
 
 export default SignedInLinks
 
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
-const SignedInLinks = ()=>{
-    return(
-        <ul className="right">
-            <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><NavLink to='/'>Log Out</NavLink></li>
-            <li><NavLink to='/' className='btn btn-floating orange lighten-1'>HY</NavLink></li>
-        </ul>
-    )
-}
-
-export default SignedInLinks
