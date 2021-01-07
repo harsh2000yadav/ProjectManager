5811fdea7e2e550c77f72eebfcb591236885e77c
Harsh <32234204+harsh2000yadav@users.noreply.github.com>
2021-01-07 21:40:32 +0530

    Update SignedOutLinks.js

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -11,3 +11,17 @@ const SignedOutLinks = ()=>{
 }
 
 export default SignedOutLinks
+
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const SignedOutLinks = ()=>{
+    return(
+        <ul className="right">
+            <li><NavLink to='/signup'>Sign Up</NavLink></li>
+            <li><NavLink to='/signin'>Login</NavLink></li>
+        </ul>
+    )
+}
+
+export default SignedOutLinks
