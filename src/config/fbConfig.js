7a476078355043da83de4a79eacd2204aa4611c5
Harsh <32234204+harsh2000yadav@users.noreply.github.com>
2021-01-13 21:37:38 +0530

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -19,25 +19,3 @@ import 'firebase/auth'
 
 
   export default firebase;
-
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
-
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyCfnnDCkQFT3aXkfxjYQxv86NGRicXJ_yg",
-    authDomain: "projectmanager-41045.firebaseapp.com",
-    projectId: "projectmanager-41045",
-    storageBucket: "projectmanager-41045.appspot.com",
-    messagingSenderId: "1021893533779",
-    appId: "1:1021893533779:web:72d5842ddda8fda88826a6",
-    measurementId: "G-1P37PRYHXW"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.firestore().settings({timestampsInSnapshots: true});
-
-
-  export default firebase;
